ff263c10f92d5083ae1951e4f190fd334a23e709
212105@fhwn.ac.at# <212105@fhwn.ac.at>
2025-04-10 10:17:16 +0200

    Programm versucht

--- pfusch.c
+++ pfusch.c
@@ -1,7 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <stdbool.h>
+#include <string.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+typedef struct command_line_arguments
+{
+    int i;
+    char const* s;
+    bool b;
+} cli_args;
+
+int main(int argc, char* argv[]) {
+    cli_args args = {0, NULL, false};
+    int optgot = -1;
+    char* optstr = "i:s:b";
+
+    do
+    {
+        optgot = getopt(argc, argv, optstr);
+        switch (optgot)
+        {
+            case 'i':
+                args.i = atoi(optarg);
+                break;
+            case 's':
+                args.s = optarg;
+                break;
+            case 'b':
+                args.b = true;
+                break;
+            case '?':
+                printf("falsches Format\n");
+                exit(EXIT_FAILURE);
+                break;
+            default:
+                break;
+        }
+    } while (optgot != -1);
+    
+
+    //char const* s = NULL;
+
+    printf("i: %d, s: %s, b: %d\n", args.i, args.s, args.b);
 
     return 0;
 }
